23e9bcbf6b13a97f1154dedfa095dd149fa51906
Richard Novoa <56084025+richardNovoa@users.noreply.github.com>
2023-03-06 11:48:01 -0500

    nextJS app

---
+++ dashboard-pilot/src/pages/index.js
@@ -0,0 +1,10 @@
+import { Button } from 'richards-dribbble-design-system';
+
+export default function Home() {
+	return (
+		<>
+			<Button label='Primary' type='primary' />
+			<Button label='Secondary' type='secondary' />
+		</>
+	);
+}
